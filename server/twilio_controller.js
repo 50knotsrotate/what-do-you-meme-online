60fc0903091758852c13ef6928d01dee48d29f09
= <=>
2019-07-01 06:11:20 -0400

    twilio

--- server/twilio_controller.js
+++ server/twilio_controller.js
@@ -1,9 +1,6 @@
 function sendMessage(data, socket, AUTH, SID, number) {
     const client = require("twilio")(SID, AUTH);
     const { pin, username } = data;
-    
-    console.log(pin);
-    console.log(username)
 
     client.messages.create({
       body: `${username} wants to play what do you meme in lobby ${pin}!`,
