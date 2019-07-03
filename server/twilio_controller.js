903d8b7536b839f95a8f711091218d4a9810b064
= <=>
2019-07-03 13:46:47 -0400

    fixed text message error

--- server/twilio_controller.js
+++ server/twilio_controller.js
@@ -1,7 +1,7 @@
 function sendMessage(data, socket, AUTH, SID, number) {
     const client = require("twilio")(SID, AUTH);
     const { pin, username } = data;
-
+  console.log(username);
     client.messages.create({
       body: `${username} wants to play what do you meme in lobby ${pin}!`,
       from: "+12183668652",
